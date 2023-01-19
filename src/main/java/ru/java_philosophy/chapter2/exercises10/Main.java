135909e1641a2cc066fba624478a207c5702732a
ruaphnb <Nikolay.PUSHKAREV@raiffeisen.ru>
2023-01-19 16:56:24 +0300

    add chapter 2 until Exercises #9

---
+++ src/main/java/ru/java_philosophy/chapter2/exercises10/Main.java
@@ -0,0 +1,11 @@
+package ru.java_philosophy.chapter2.exercises10;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] args2 = new String[]{"param", "param2", "param3"};
+        for (String str : args2) {
+            System.out.println("arg = " + str);
+        }
+    }
+}
