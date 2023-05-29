cde3ece35f45955c385e33200df00618e97d08bd
ruaphnb <Nikolay.PUSHKAREV@raiffeisen.ru>
2023-05-29 10:32:10 +0300

    java philosophy 21 chapter - Присоединение к потоку

---
+++ src/main/java/ru/java_philosophy/chapter21/page914/Sleeper.java
@@ -0,0 +1,24 @@
+package ru.java_philosophy.chapter21.page914;
+
+import java.time.LocalDateTime;
+
+public class Sleeper extends Thread {
+
+    private int duration;
+
+    public Sleeper(String name, int duration){
+        super(name);
+        this.duration = duration;
+        start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            sleep(duration);
+        }catch (InterruptedException ie){
+            System.out.println("Sleeper InterruptedException " + ie.getMessage());
+        }
+        System.out.println(getName() + " has awakened " + LocalDateTime.now());
+    }
+}
