c2296a65c1b724cab9c8f789324b5b68d420ab30
ruaphnb <Nikolay.PUSHKAREV@raiffeisen.ru>
2023-05-31 10:33:47 +0300

    thinking in java 21 chapter - Синхронизация EvenGenerator + Использование объеков Lock

--- src/main/java/ru/java_philosophy/chapter21/page920/EvenChecker.java
+++ src/main/java/ru/java_philosophy/chapter21/page920/EvenChecker.java
@@ -2,11 +2,15 @@ package ru.java_philosophy.chapter21.page920;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import ru.java_philosophy.chapter21.IntGenerator;
 
 public class EvenChecker implements Runnable {
 
     private IntGenerator generator;
+
+    private Lock lock = new ReentrantLock();
 
     public EvenChecker(IntGenerator g) {
         this.generator = g;
@@ -14,25 +18,33 @@ public class EvenChecker implements Runnable {
 
     @Override
     public void run() {
+        int x;
         while (!generator.isCanceled()) {
-            int x = generator.next();
-            if (x % 2 != 0) {
-                System.out.println("Thread " + Thread.currentThread().getName());
-                System.out.println("Wow generator.next() exception rice conditions");
-                generator.cancel();
+            lock.lock();
+            try {
+                x = generator.next();
+
+                if (x % 2 != 0 || x > 1000) {
+                    System.out.println("Thread " + Thread.currentThread().getName());
+                    System.out.println("Wow generator.next() exception rice conditions");
+                    generator.cancel();
+                }
+                System.out.println(x + " Thread " + Thread.currentThread().getName());
+            } finally {
+                lock.unlock();
             }
         }
     }
 
-    public static void test(IntGenerator g, int count){
+    public static void test(IntGenerator g, int count) {
         ExecutorService exec = Executors.newCachedThreadPool();
-        for(int i=0; i< count; i++){
+        for (int i = 0; i < count; i++) {
             exec.execute(new EvenChecker(g));
         }
         exec.shutdown();
     }
 
-    public static void test(IntGenerator g){
-        test(g, 10);
+    public static void test(IntGenerator g) {
+        test(g, 3);
     }
 }
