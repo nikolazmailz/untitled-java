91fe2e68862e9502574f9366600152a3e48302bc
ruaphnb <Nikolay.PUSHKAREV@raiffeisen.ru>
2023-06-08 09:32:57 +0300

    thinking in java 21 chapter - Синхронизация EvenGenerator + Использование объеков Lock

--- src/main/java/ru/java_philosophy/chapter21/page920/EvenChecker.java
+++ src/main/java/ru/java_philosophy/chapter21/page920/EvenChecker.java
@@ -2,15 +2,11 @@ package ru.java_philosophy.chapter21.page920;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 import ru.java_philosophy.chapter21.IntGenerator;
 
 public class EvenChecker implements Runnable {
 
     private IntGenerator generator;
-
-    private Lock lock = new ReentrantLock();
 
     public EvenChecker(IntGenerator g) {
         this.generator = g;
@@ -20,19 +16,13 @@ public class EvenChecker implements Runnable {
     public void run() {
         int x;
         while (!generator.isCanceled()) {
-            lock.lock();
-            try {
-                x = generator.next();
-
-                if (x % 2 != 0 || x > 1000) {
-                    System.out.println("Thread " + Thread.currentThread().getName());
-                    System.out.println("Wow generator.next() exception rice conditions");
-                    generator.cancel();
-                }
-                System.out.println(x + " Thread " + Thread.currentThread().getName());
-            } finally {
-                lock.unlock();
+            x = generator.next();
+            if (x % 2 != 0 || x > 1000) {
+                System.out.println("Thread " + Thread.currentThread().getName());
+                System.out.println("Wow generator.next() exception rice conditions");
+                generator.cancel();
             }
+            System.out.println(x + " Thread " + Thread.currentThread().getName());
         }
     }
 
