228622b8927ba8a4506f7e545b327ab354814eee
ruaphnb <Nikolay.PUSHKAREV@raiffeisen.ru>
2023-06-14 17:49:57 +0300

    thinking in java 21 chapter - Критические секции

---
+++ src/main/java/ru/java_philosophy/_check_static/ClassA.java
@@ -0,0 +1,30 @@
+package ru.java_philosophy._check_static;
+
+public class ClassA {
+
+    public static class ClassB {
+        public int i;
+        public static int j;
+
+        public ClassB(int i, int j){
+            this.i = i;
+            this.j = j;
+        }
+
+        public String toString(){
+            return "i " + i + ", j " + j;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ClassA.ClassB b1 = new ClassA.ClassB(1, 0);
+        ClassA.ClassB b2 = new ClassA.ClassB(2, 1);
+        ClassA.ClassB b3 = new ClassA.ClassB(3, 2);
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+
+    }
+}
