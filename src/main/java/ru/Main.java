1a1ca1ec79de62ac9b5644e4eea87f30778faf30
Pushkarev Nikolay <nikolay.pushkarev@raiffeisen.ru>
2024-07-17 13:01:41 +0300

    leet code

--- src/main/java/ru/Main.java
+++ src/main/java/ru/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String s1 = new String("hello");
+        String s2 = "hello";
+
+        System.out.println(s1 == s2);
+
     }
 }
