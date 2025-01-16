512ce1a4517280b01b917f518c9f6bbaff9fb409
Pushkarev Nikolay <nikolazmailz@gmail.com>
2025-01-16 08:32:52 +0300

    add

--- src/main/java/ru/leetcode/algorithms/ThirdMaximumNumber_414/Solution.java
+++ src/main/java/ru/leetcode/algorithms/ThirdMaximumNumber_414/Solution.java
@@ -14,17 +14,16 @@ public class Solution {
     public int thirdMax(int[] nums) {
         int max1 = 0, max2 = 0, max3 = 0;
         int maxR = 0;
-        for(int i = 0; i < nums.length; i++){
-            int num = nums[i];
-            if(num >= max1){
-                if(num > max2){
+        for (int num : nums) {
+            if (num >= max1) {
+                if (num > max2) {
                     max2 = max1;
                 }
                 max1 = num;
                 maxR = num;
-            } else if(num < max1 && num >= max2) {
+            } else if (num >= max2) {
                 max2 = num;
-            } else if(num < max2 && num >= max3){
+            } else if (num >= max3) {
                 max3 = num;
                 maxR = num;
             }
